26f234ea21039a8b67203c5bd34df5f9a859bd2b
Justin Mullis <justin.mullis@gmail.com>
2019-11-16 12:34:41 -0500

    Bootstrap products

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,24 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import '../stylesheets/application'
+import 'bootstrap/js/dist/alert'
+import 'bootstrap/js/dist/button'
+import 'bootstrap/js/dist/carousel'
+import 'bootstrap/js/dist/collapse'
+import 'bootstrap/js/dist/dropdown'
+import 'bootstrap/js/dist/index'
+import 'bootstrap/js/dist/modal'
+import 'bootstrap/js/dist/popover'
+import 'bootstrap/js/dist/scrollspy'
+import 'bootstrap/js/dist/tab'
+import 'bootstrap/js/dist/toast'
+import 'bootstrap/js/dist/tooltip'
+import 'bootstrap/js/dist/util'
